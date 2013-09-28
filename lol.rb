5221f13ff8710416912f8546d755d7dd1320089f
Ng Guoyou <fauxface+github@gmail.com>
2013-09-29 03:34:34 +0800

    Expand Ruby app to also take in JSON; add statistics.

--- lol.rb
+++ lol.rb
@@ -11,32 +11,66 @@ get "/" do
 end
 
 post '/upload' do
-  puts params.inspect
-  url = params[:post]["input"]
-  tempjson = open(url).read;
+  begin
+    input = params[:post]["input"]
 
-  if JSON.parse(tempjson)
+    if input.is_json?
+      tempjson = input
+    else
+      # It's a URL
+      tempjson = open(input).read
+      if !tempjson.is_json?
+        # Not JSON, invalid JSON at URL
+        redirect "/"
+      end
+    end
+
+    # Valid JSON
     filename = Digest::SHA256.hexdigest(tempjson)
-    File.open("public/uploads/lists/#{filename}.json", 'w') do |f|
+    path = "uploads/lists/#{filename}.json"
+
+    File.open("public/#{path}", 'w') do |f|
       f.puts tempjson
     end
+
+    # Update newest uploads
+    File.open("public/latest.json", "w+") do |f|
+      list = JSON.parse(tempjson)
+
+      begin
+        latest = JSON.parse(f.read)
+      rescue
+        latest = []
+      end
+
+      latest = latest.take(9).push({ "path" => path.to_s, "title" => list['title'].to_s })
+      f.puts latest.to_json
+    end
+
+    # Update statistics
+    File.open("public/statistics.json", "w+") do |f|
+      begin
+        stats = JSON.parse(f.read)
+      rescue
+        stats = { uploads: 0 }
+      end
+
+      stats[:uploads] += 1
+      f.puts stats.to_json
+    end
+
     redirect "/index.html#uploads/lists/#{filename}.json"
+  rescue
+    redirect "/"
   end
 end
 
-
-# post '/upload' do
-#   puts "LOL"
-#   unless params[:file] &&
-#          (tmpfile = params[:file][:tempfile]) &&
-#          (name = params[:file][:filename])
-#     @error = "No file selected"
-#     return haml(:upload)
-#   end
-#   STDERR.puts "Uploading file, original name #{name.inspect}"
-#   while blk = tmpfile.read(65536)
-#     # here you would write it to its final location
-#     STDERR.puts blk.inspect
-#   end
-#   "Upload complete"
-# end
+class String
+  def is_json?
+    begin
+      !!JSON.parse(self)
+    rescue
+      false
+    end
+  end
+end
